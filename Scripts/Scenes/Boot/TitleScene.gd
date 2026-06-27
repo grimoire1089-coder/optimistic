01e46eb2b1832bd53d893f6c7fbd1ee79c141237
Grimgift <grimoire1089@gmail.com>
2026-06-28 08:09:43 +0900

    Wire title scene audio hooks

--- Scripts/Scenes/Boot/TitleScene.gd
+++ Scripts/Scenes/Boot/TitleScene.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@export var title_bgm: AudioStream
+@export var start_button_sfx: AudioStream
+@export var exit_button_sfx: AudioStream
+
 @onready var start_button: Button = $CenterContainer/CenterBox/StartButton
 @onready var exit_button: Button = $CenterContainer/CenterBox/ExitButton
 
@@ -11,13 +15,22 @@ func _ready() -> void:
 	start_button.pressed.connect(_on_start_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
+	if title_bgm != null:
+		AudioPlayer.play_bgm(title_bgm)
+
 
 func _on_start_button_pressed() -> void:
 	start_button.disabled = true
 	exit_button.disabled = true
 
+	if start_button_sfx != null:
+		AudioPlayer.play_sfx(start_button_sfx)
+
 	SceneRouter.go_to_main()
 
 
 func _on_exit_button_pressed() -> void:
+	if exit_button_sfx != null:
+		AudioPlayer.play_sfx(exit_button_sfx)
+
 	get_tree().quit()
