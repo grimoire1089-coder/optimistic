ec4c98b827fd4203986db57e0a5dcdba1eb1d702
Grimgift <grimoire1089@gmail.com>
2026-06-29 08:16:37 +0900

    Add need action id constants

---
+++ Scripts/Systems/Needs/CharacterNeedActionIds.gd
@@ -0,0 +1,9 @@
+extends RefCounted
+class_name CharacterNeedActionIds
+
+const EAT := &"need_action_eat"
+const REST := &"need_action_rest"
+const MAINTAIN := &"need_action_maintain"
+const PLAY := &"need_action_play"
+const CHAT := &"need_action_chat"
+const IDLE := &"need_action_idle"
