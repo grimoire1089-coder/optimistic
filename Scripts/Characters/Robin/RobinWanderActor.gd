3529156d722c4481434a1ca8a79593b93e6c7db2
Grimgift <grimoire1089@gmail.com>
2026-06-29 05:50:28 +0900

    Start Robin at movement area center

--- Scripts/Characters/Robin/RobinWanderActor.gd
+++ Scripts/Characters/Robin/RobinWanderActor.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 class_name RobinWanderActor
 
+@export var start_at_movement_area_center: bool = true
+
 @onready var wander_module: RobinRandomWanderModule = $RobinRandomWanderModule
 @onready var walk_animator: RobinWalkSpriteAnimator = $RobinWalkSpriteAnimator
 
 
 func _ready() -> void:
 	wander_module.setup(self)
+	if start_at_movement_area_center:
+		global_position = wander_module.get_movement_center()
 	walk_animator.setup()
 
 
