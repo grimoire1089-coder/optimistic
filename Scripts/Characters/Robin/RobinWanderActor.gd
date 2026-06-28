088a1449c3727c32db0d08a2858b3d0234553d77
Grimgift <grimoire1089@gmail.com>
2026-06-29 08:38:35 +0900

    Expose Robin needs module references

--- Scripts/Characters/Robin/RobinWanderActor.gd
+++ Scripts/Characters/Robin/RobinWanderActor.gd
@@ -3,6 +3,9 @@ class_name RobinWanderActor
 
 @export var start_at_movement_area_center: bool = true
 
+@onready var needs_bundle: Node = $AICharacterNeedsBundle
+@onready var needs_module: CharacterNeedsModule = $AICharacterNeedsBundle/CharacterNeedsModule
+@onready var need_planner: NeedDrivenAIPlanner = $AICharacterNeedsBundle/NeedDrivenAIPlanner
 @onready var wander_module: RobinRandomWanderModule = $RobinRandomWanderModule
 @onready var walk_animator: RobinWalkSpriteAnimator = $RobinWalkSpriteAnimator
 
@@ -24,3 +27,23 @@ func _physics_process(delta: float) -> void:
 
 func get_movement_area() -> Rect2:
 	return wander_module.get_movement_area()
+
+
+func get_needs_module() -> CharacterNeedsModule:
+	return needs_module
+
+
+func get_need_planner() -> NeedDrivenAIPlanner:
+	return need_planner
+
+
+func get_current_need_action_id() -> StringName:
+	if need_planner == null:
+		return CharacterNeedActionIds.IDLE
+	return need_planner.get_next_action_id()
+
+
+func get_current_lowest_need_id() -> StringName:
+	if needs_module == null:
+		return &""
+	return needs_module.get_lowest_need_id()
