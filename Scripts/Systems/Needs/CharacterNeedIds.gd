1607686526e6036a508b5695c1701bd888290509
Grimgift <grimoire1089@gmail.com>
2026-06-29 07:53:35 +0900

    Add character need id constants

---
+++ Scripts/Systems/Needs/CharacterNeedIds.gd
@@ -0,0 +1,22 @@
+extends RefCounted
+class_name CharacterNeedIds
+
+const HUNGER := &"hunger"
+const ENERGY := &"energy"
+const HYGIENE := &"hygiene"
+const FUN := &"fun"
+const SOCIAL := &"social"
+
+const DEFAULT_DEFINITION_PATHS := [
+	"res://Data/Needs/Definitions/hunger.tres",
+	"res://Data/Needs/Definitions/energy.tres",
+	"res://Data/Needs/Definitions/hygiene.tres",
+	"res://Data/Needs/Definitions/fun.tres",
+	"res://Data/Needs/Definitions/social.tres",
+]
+
+static func get_default_definition_paths() -> Array[String]:
+	var result: Array[String] = []
+	for path in DEFAULT_DEFINITION_PATHS:
+		result.append(path)
+	return result
