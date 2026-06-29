ca043a2ece59f8aac87b184d9ebf7b94e6439a52
Grimgift <grimoire1089@gmail.com>
2026-06-30 06:59:43 +0900

    Expand food item resource fields

--- Scripts/Items/Food/FoodItemData.gd
+++ Scripts/Items/Food/FoodItemData.gd
@@ -1,2 +1,17 @@
 extends Resource
 class_name FoodItemData
+
+@export var item_id: StringName = &""
+@export var display_name: String = ""
+@export_multiline var description: String = ""
+@export var icon: Texture2D
+@export var category_id: StringName = &"foods"
+@export_range(1, 999, 1) var stack_max: int = 99
+@export_range(0, 999999, 1) var buy_price: int = 0
+@export_range(0, 999999, 1) var sell_price: int = 0
+
+
+func get_icon_path() -> String:
+	if icon == null:
+		return ""
+	return icon.resource_path
