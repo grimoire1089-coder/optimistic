51f2ce03fb05509973a7775e5ce92b25ffdda034
Grimgift <grimoire1089@gmail.com>
2026-06-29 09:34:27 +0900

    Make work button open work menu

--- Scripts/UI/HUD/WorkCreditButton.gd
+++ Scripts/UI/HUD/WorkCreditButton.gd
@@ -1,45 +1,19 @@
 extends Button
 class_name WorkCreditButton
 
-@export var reward_amount: int = 25
-@export var reward_reason: String = "work_button"
-@export var cooldown_seconds: float = 1.0
 @export var label_text: String = "仕事"
-
-var _cooldown_left: float = 0.0
+@export var work_menu_path: NodePath = NodePath("../WorkMenu")
 
 
 func _ready() -> void:
+	text = label_text
 	pressed.connect(_on_pressed)
-	_refresh_label()
-
-
-func _process(delta: float) -> void:
-	if _cooldown_left <= 0.0:
-		return
-
-	_cooldown_left = maxf(_cooldown_left - delta, 0.0)
-	disabled = _cooldown_left > 0.0
-	_refresh_label()
 
 
 func _on_pressed() -> void:
-	if _cooldown_left > 0.0:
+	var work_menu := get_node_or_null(work_menu_path) as WorkMenu
+	if work_menu == null:
+		push_warning("仕事UIが見つかりません: %s" % work_menu_path)
 		return
 
-	var added := CreditWallet.add(reward_amount, reward_reason)
-	if not added:
-		return
-
-	_cooldown_left = cooldown_seconds
-	disabled = true
-	_refresh_label()
-
-
-func _refresh_label() -> void:
-	if _cooldown_left > 0.0:
-		text = "%s\n+%d CR\n%.1fs" % [label_text, reward_amount, _cooldown_left]
-		return
-
-	disabled = false
-	text = "%s\n+%d CR" % [label_text, reward_amount]
+	work_menu.open_menu()
