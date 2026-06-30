2a26fc4f23994c69fc898d3374f78d27c619715b
Grimgift <grimoire1089@gmail.com>
2026-06-30 23:07:29 +0900

    Add mood entry body field

--- Scripts/Systems/Mood/CharacterMoodEntryData.gd
+++ Scripts/Systems/Mood/CharacterMoodEntryData.gd
@@ -4,5 +4,6 @@ class_name CharacterMoodEntryData
 @export var entry_id: StringName = &""
 @export var display_name: String = ""
 @export var detail_text: String = ""
+@export var body: String = ""
 @export var point: int = 0
 @export var until_next_season: bool = false
