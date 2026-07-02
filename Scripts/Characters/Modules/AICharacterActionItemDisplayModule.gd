93ea673d84fa418081da57c8ee6db57cbe5fc9a7
Grimgift <grimoire1089@gmail.com>
2026-07-03 01:28:06 +0900

    Snap standing lapis transitions from action item display

--- Scripts/Characters/Modules/AICharacterActionItemDisplayModule.gd
+++ Scripts/Characters/Modules/AICharacterActionItemDisplayModule.gd
@@ -7,6 +7,7 @@ class_name AICharacterActionItemDisplayModule
 @export var item_center_offset: Vector2 = Vector2(0.0, -18.0)
 @export var item_display_size: Vector2 = Vector2(70.0, 70.0)
 @export var item_z_index: int = 190
+@export var snap_standing_lapis_to_grid: bool = true
 
 var _body: Node2D
 var _hydrate_behavior: Node
@@ -15,11 +16,13 @@ var _sit_behavior: Node
 var _item_rect: TextureRect
 var _display_add_deferred := false
 var _current_icon_path := ""
+var _was_standing_lapis := false
 
 
 func _ready() -> void:
 	_body = get_parent() as Node2D
 	_resolve_refs()
+	_was_standing_lapis = _is_standing_lapis_active()
 	_request_display()
 	set_process(true)
 
@@ -27,11 +30,13 @@ func _ready() -> void:
 func setup(body: Node2D) -> void:
 	_body = body
 	_resolve_refs()
+	_was_standing_lapis = _is_standing_lapis_active()
 	_request_display()
 
 
 func _process(_delta: float) -> void:
 	_resolve_refs()
+	_snap_lapis_transition_if_needed()
 	_request_display()
 	_update_display()
 
@@ -60,6 +65,36 @@ func _update_display() -> void:
 	_item_rect.visible = _item_rect.texture != null
 	_item_rect.size = item_display_size
 	_item_rect.position = item_center_offset - item_display_size * 0.5
+
+
+func _snap_lapis_transition_if_needed() -> void:
+	if not snap_standing_lapis_to_grid:
+		_was_standing_lapis = false
+		return
+	var is_standing_lapis := _is_standing_lapis_active()
+	if is_standing_lapis != _was_standing_lapis:
+		_snap_body_to_grid()
+	_was_standing_lapis = is_standing_lapis
+
+
+func _is_standing_lapis_active() -> bool:
+	if _sit_behavior == null:
+		return false
+	if not _sit_behavior.has_method("is_using_lapis"):
+		return false
+	if _sit_behavior.call("is_using_lapis") != true:
+		return false
+	if _sit_behavior.has_method("is_sitting") and _sit_behavior.call("is_sitting") == true:
+		return false
+	return true
+
+
+func _snap_body_to_grid() -> void:
+	if _body == null:
+		return
+	if not _body.has_method("snap_to_nearest_walkable_grid"):
+		return
+	_body.call("snap_to_nearest_walkable_grid")
 
 
 func _get_active_item_source() -> Node:
