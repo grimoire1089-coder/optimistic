a7cb9f4712a0d7286e53ddf0dd5717be198aa4a9
Grimgift <grimoire1089@gmail.com>
2026-07-02 12:34:14 +0900

    Add craft time to recipe data

--- Scripts/Craft/CraftRecipeData.gd
+++ Scripts/Craft/CraftRecipeData.gd
@@ -3,6 +3,7 @@ class_name CraftRecipeData
 
 @export var recipe_id: StringName = &""
 @export var category_id: StringName = &"cooking"
+@export_range(1, 9999, 1) var craft_game_minutes: int = 1
 @export var output_item: FoodItemData
 @export var output_amount: int = 1
 @export var ingredients: Array[CraftIngredientData] = []
