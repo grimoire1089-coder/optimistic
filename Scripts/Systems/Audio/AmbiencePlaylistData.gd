b16a2e2c627f4b795b7d6c3cbaa8aecc255458e7
Grimgift <grimoire1089@gmail.com>
2026-07-05 13:53:38 +0900

    Add ambience loop setting

--- Scripts/Systems/Audio/AmbiencePlaylistData.gd
+++ Scripts/Systems/Audio/AmbiencePlaylistData.gd
@@ -7,6 +7,7 @@ class_name AmbiencePlaylistData
 @export var shuffle_tracks: bool = false
 @export_range(0, 999, 1) var start_index: int = 0
 @export var restart_if_same: bool = false
+@export var loop_tracks: bool = true
 @export_range(0.0, 10.0, 0.05) var fade_out_seconds: float = 0.5
 @export_range(0.0, 10.0, 0.05) var fade_in_seconds: float = 0.5
 
