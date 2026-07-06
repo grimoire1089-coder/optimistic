7945c816f0215678d7a5432723cee7e1c20b9e7b
Grimgift <grimoire1089@gmail.com>
2026-07-06 15:17:51 +0900

    Swap build and placeholder HUD button positions

--- Scripts/UI/HUD/Modules/RightHudLayoutModule.gd
+++ Scripts/UI/HUD/Modules/RightHudLayoutModule.gd
@@ -25,7 +25,7 @@ const BUTTON_LAYOUTS := {
 	"ShopButton": Vector2(HudButtonStyle.FIRST_ROW_SHOP_LEFT, HudButtonStyle.FIRST_ROW_TOP),
 	"BookButton": Vector2(HudButtonStyle.FIRST_ROW_BOOK_LEFT, HudButtonStyle.FIRST_ROW_TOP),
 	"InventoryButton": Vector2(HudButtonStyle.FIRST_ROW_INVENTORY_LEFT, HudButtonStyle.FIRST_ROW_TOP),
-	"BuildModeButton": Vector2(HudButtonStyle.FIRST_ROW_BUILD_LEFT, HudButtonStyle.FIRST_ROW_TOP),
+	"BuildModeButton": Vector2(HudButtonStyle.THIRD_ROW_PLACEHOLDER_03_LEFT, HudButtonStyle.THIRD_ROW_TOP),
 	"MoveButton": Vector2(HudButtonStyle.SECOND_ROW_MOVE_LEFT, HudButtonStyle.SECOND_ROW_TOP),
 	"WorkCreditButton": Vector2(HudButtonStyle.SECOND_ROW_WORK_LEFT, HudButtonStyle.SECOND_ROW_TOP),
 	"CraftButton": Vector2(HudButtonStyle.SECOND_ROW_CRAFT_LEFT, HudButtonStyle.SECOND_ROW_TOP),
@@ -33,7 +33,7 @@ const BUTTON_LAYOUTS := {
 	"BillButton": Vector2(HudButtonStyle.SECOND_ROW_BILL_LEFT, HudButtonStyle.SECOND_ROW_TOP),
 	"PlanHudButton": Vector2(HudButtonStyle.THIRD_ROW_PLACEHOLDER_01_LEFT, HudButtonStyle.THIRD_ROW_TOP),
 	"PlaceholderHudButton02": Vector2(HudButtonStyle.THIRD_ROW_PLACEHOLDER_02_LEFT, HudButtonStyle.THIRD_ROW_TOP),
-	"PlaceholderHudButton03": Vector2(HudButtonStyle.THIRD_ROW_PLACEHOLDER_03_LEFT, HudButtonStyle.THIRD_ROW_TOP),
+	"PlaceholderHudButton03": Vector2(HudButtonStyle.FIRST_ROW_BUILD_LEFT, HudButtonStyle.FIRST_ROW_TOP),
 	"EncyclopediaButton": Vector2(HudButtonStyle.THIRD_ROW_PLACEHOLDER_04_LEFT, HudButtonStyle.THIRD_ROW_TOP),
 	"PlaceholderHudButton05": Vector2(HudButtonStyle.SECOND_ROW_SETTINGS_LEFT, HudButtonStyle.SECOND_ROW_TOP),
 }
