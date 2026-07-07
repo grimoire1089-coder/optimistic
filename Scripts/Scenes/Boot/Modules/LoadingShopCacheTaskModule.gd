f57b30f8ca158846bf3ecfe01ca8d0865ab8b940
Grimgift <grimoire1089@gmail.com>
2026-07-07 22:42:42 +0900

    Add loading shop cache task module

---
+++ Scripts/Scenes/Boot/Modules/LoadingShopCacheTaskModule.gd
@@ -0,0 +1,12 @@
+extends LoadingTaskModule
+class_name LoadingShopCacheTaskModule
+
+
+func _ready() -> void:
+	task_id = &"shop_cache"
+	display_name = "ショップデータ準備中..."
+	weight = 1.0
+
+
+func run_task(_context: Dictionary = {}) -> void:
+	ShopRuntimeCache.prepare_default_database()
